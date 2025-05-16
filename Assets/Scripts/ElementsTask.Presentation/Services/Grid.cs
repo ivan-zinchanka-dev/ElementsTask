aacc06229812a60fe8f9893f2379a7932fb64590
Ivan Zinchanka <75093640+ivan-zinchanka-dev@users.noreply.github.com>
2025-05-16 18:30:23 +0300

    feat: grid, p.1

--- Assets/Scripts/ElementsTask.Presentation/Services/Grid.cs
+++ Assets/Scripts/ElementsTask.Presentation/Services/Grid.cs
@@ -4,23 +4,42 @@ namespace ElementsTask.Presentation.Services
 {
     public class Grid : MonoBehaviour
     {
-        [SerializeField] 
-        private float _cellSize = 1.0f;
+        [SerializeField]
+        private Vector2 _cellSize = new Vector2(2, 2);
+        [SerializeField]
+        private Vector2Int _cellsCount = new Vector2Int(6, 9);
         
         private Transform[,] _cells;
         
         [Sirenix.OdinInspector.Button]
-        private void Generate(int cellsCount)
+        private void Generate()
         {
-            Vector3 origin = transform.localPosition;
-
+            Vector3 originPosition = transform.position;
+            Vector3 currentPosition = originPosition;
+            
             Debug.Log("Generate");
             
-            /*for (int i = 0; i < UPPER; i++)
+            for (int i = 0; i < _cellsCount.y; i++)
             {
-                
-            }*/
+                for (int j = 0; j < _cellsCount.x; j++)
+                {
+                    var go = new GameObject("w");
+                    go.transform.SetParent(transform);
+                    go.transform.position = currentPosition;
 
+                    currentPosition.x += _cellSize.x;
+                }
+
+                currentPosition.x = originPosition.x;
+                currentPosition.y += _cellSize.y;
+            }
+
+        }
+
+        [Sirenix.OdinInspector.Button]
+        private void Clear()
+        {
+            
         }
 
     }
