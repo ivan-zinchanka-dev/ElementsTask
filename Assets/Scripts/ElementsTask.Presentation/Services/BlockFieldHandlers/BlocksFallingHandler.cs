4e6e58de2300d012a6533e088f2da2aea6831102
Ivan Zinchanka <75093640+ivan-zinchanka-dev@users.noreply.github.com>
2025-05-17 20:12:41 +0300

    feat: falling, p.2

--- Assets/Scripts/ElementsTask.Presentation/Services/BlockFieldHandlers/BlocksFallingHandler.cs
+++ Assets/Scripts/ElementsTask.Presentation/Services/BlockFieldHandlers/BlocksFallingHandler.cs
@@ -2,6 +2,7 @@
 using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using ElementsTask.Common.Extensions;
+using ElementsTask.Presentation.Models;
 using ElementsTask.Presentation.Views;
 using UnityEngine;
 
@@ -15,7 +16,7 @@ namespace ElementsTask.Presentation.Services.BlockFieldHandlers
 
         private Sequence _fallingTween;
         
-        public BlocksFallingHandler(Vector2Int fieldSize, List<BlockView> blocks, float fallingSpeed = 1f)
+        public BlocksFallingHandler(Vector2Int fieldSize, List<BlockView> blocks, float fallingSpeed = 3f)
         {
             _fieldSize = fieldSize;
             _blocks = blocks;
@@ -29,38 +30,85 @@ namespace ElementsTask.Presentation.Services.BlockFieldHandlers
                 return;
             }
 
-            List<BlockView> relevantBlocks = GetRelevantBlocks();
+            Dictionary<BlockView, BlockView> swapPairs = GetSwapPairs();
 
             _fallingTween = DOTween.Sequence();
 
-            foreach (BlockView block in relevantBlocks)
+            foreach (var pair in swapPairs)
             {
-                _fallingTween
-                    .Join(block.transform.DOShakeScale(0.15f, new Vector3(1, 1, 0)));
+                _fallingTween.Join(BeginSwap(pair.Key, pair.Value));
             }
 
             await _fallingTween.ToUniTask();
         }
 
-        private List<BlockView> GetRelevantBlocks()
+        private Dictionary<BlockView, BlockView> GetSwapPairs()
         {
-            List<BlockView> relevantBlocks = new List<BlockView>(_blocks.Capacity);
+            var swapPairs = new Dictionary<BlockView, BlockView>();
             
             foreach (BlockView block in _blocks)
             {
                 if (!block.IsEmpty && block.GridPosition.y > 0)
                 {
-                    Vector2Int bottom = block.GridPosition.WithY(block.GridPosition.y - 1);
-                    BlockView blockBottom = _blocks.Find(other => other.GridPosition == bottom);
-
-                    if (blockBottom != null && blockBottom.IsEmpty)
+                    BlockView pair = GetSwapPair(block);
+                    
+                    if (pair != null)
                     {
-                        relevantBlocks.Add(block);
+                        swapPairs.Add(block, pair);
+                        
+                        //relevantBlocks.Add(block);
                     }
                 }
             }
 
-            return relevantBlocks;
+            return swapPairs;
+        }
+
+        private BlockView GetSwapPair(BlockView origin)
+        {
+            Vector2Int targetPosition = origin.GridPosition.WithY(origin.GridPosition.y - 1);
+            BlockView pair = null;
+
+            while (targetPosition.y >= 0)
+            {
+                BlockView current = _blocks.Find(other => other.GridPosition == targetPosition);
+
+                if (!current.IsEmpty)
+                {
+                    break;
+                }
+
+                pair = current;
+                targetPosition.y--;
+            }
+
+            return pair;
+        }
+        
+        //TODO Unify
+        private Tween BeginSwap(BlockView first, BlockView second)
+        {
+            BlockSwapData firstData = first.GetSwapData();
+            BlockSwapData secondData = second.GetSwapData();
+            
+            first
+                .SetGridPosition(secondData.GridPosition)
+                .SetSortingOrder(secondData.SortingOrder);
+            
+            second
+                .SetGridPosition(firstData.GridPosition)
+                .SetSortingOrder(firstData.SortingOrder);
+
+            float fallingDistance = Vector3.Distance(first.transform.position, second.transform.position);
+            float fallingDuration = fallingDistance / _fallingSpeed;
+
+            return DOTween.Sequence()
+                .Append(first.transform
+                    .DOMove(secondData.WorldPosition, fallingDuration)
+                    .SetEase(Ease.InQuart))
+                .Join(second.transform
+                    .DOMove(firstData.WorldPosition, fallingDuration)
+                    .SetEase(Ease.InQuart));
         }
     }
 }
